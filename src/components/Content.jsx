63ffb25b88f996a56fe07ba4939b0c5434d196c9
HugoSbl <hugo.sablot@gmail.com>
2022-12-19 10:28:24 +0100

    update of modal

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PricingCard from "./card/pricing-card/PricingCard.jsx";
 import Comments from "./comments/Comments.jsx";
 import pricingCardItems from "./card/pricing-card/PricingData";
+import Modal from "./customComponents/Modal.jsx";
 import { Toaster } from "react-hot-toast";
 
 const Content = () => {
@@ -32,12 +33,15 @@ const Content = () => {
           },
         }}
       />
+
       <div className="flex justify-evenly">
         {pricingCardItems.map((cardItem) => (
           <PricingCard key={cardItem.id} {...cardItem} className="w-1/3" />
         ))}
       </div>
+
       <Comments />
+      <Modal />
     </>
   );
 };
