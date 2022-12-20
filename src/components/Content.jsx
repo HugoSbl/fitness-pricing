cea3f506cc8c668bda1a8510cdf49cf1803a17ca
HugoSbl <hugo.sablot@gmail.com>
2022-12-20 23:01:00 +0100

    Context added

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,44 +4,47 @@ import Comments from "./comments/Comments.jsx";
 import pricingCardItems from "./card/pricing-card/PricingData";
 import Modal from "./customComponents/Modal.jsx";
 import { Toaster } from "react-hot-toast";
+import { createContext, useState } from "react";
 
 const Content = () => {
   return (
     <>
-      <Toaster
-        position="bottom-right"
-        reverseOrder={true}
-        gutter={8}
-        containerClassName=""
-        containerStyle={{}}
-        toastOptions={{
-          // Define default options
-          className: "",
-          duration: 3000,
-          style: {
-            background: "#fff",
-            color: "#363636",
-          },
-
-          // Default options for specific types
-          success: {
+      <KnowSelectedData.Provider value={{ selectedData, setSelectedData }}>
+        <Toaster
+          position="bottom-right"
+          reverseOrder={true}
+          gutter={8}
+          containerClassName=""
+          containerStyle={{}}
+          toastOptions={{
+            // Define default options
+            className: "",
             duration: 3000,
-            theme: {
-              primary: "green",
-              secondary: "black",
+            style: {
+              background: "#fff",
+              color: "#363636",
             },
-          },
-        }}
-      />
 
-      <div className="flex justify-evenly">
-        {pricingCardItems.map((cardItem) => (
-          <PricingCard key={cardItem.id} {...cardItem} className="w-1/3" />
-        ))}
-      </div>
+            // Default options for specific types
+            success: {
+              duration: 3000,
+              theme: {
+                primary: "green",
+                secondary: "black",
+              },
+            },
+          }}
+        />
 
-      <Comments />
-      <Modal />
+        <div className="flex justify-evenly">
+          {pricingCardItems.map((cardItem) => (
+            <PricingCard key={cardItem.id} {...cardItem} className="w-1/3" />
+          ))}
+        </div>
+
+        <Comments />
+        <Modal />
+      </KnowSelectedData.Provider>
     </>
   );
 };
